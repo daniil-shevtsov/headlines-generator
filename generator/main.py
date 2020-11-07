6d2c388c0261d48da7dc1225494dbdab340a5ab4
Daniil Shevtsov <me@daniilshevtsov.com>
2020-11-07 17:22:13 +0300

    read dataset file

--- generator/main.py
+++ generator/main.py
@@ -1,5 +1,14 @@
+import pandas as pd
+
+
 def main():
     print("Hello World!")
+
+
+def read_data():
+    dataset = pd.read_csv('../generator/dataset/dataset.csv', delimiter=',')
+    data = dataset.drop(['publish_date'], axis=1).rename(columns={'headline_text': 'headline'})
+    return data
 
 
 if __name__ == "__main__":
