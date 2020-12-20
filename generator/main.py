a05dc866442b4bfd4b7a4f00b113f46e4d98acb2
Daniil Shevtsov <me@daniilshevtsov.com>
2020-12-20 21:51:59 +0300

    Add getting X, Y and tokenizer

--- generator/main.py
+++ generator/main.py
@@ -1,16 +1,37 @@
+from dataclasses import dataclass
+
 import pandas as pd
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+from keras.utils import to_categorical
 
 START = '÷'
 END = '■'
 
 
+@dataclass
+class Parameters:
+    max_features: int
+    max_len: int
+
+
+def get_parameters():
+    return Parameters(
+        max_features=3500,
+        max_len=200
+    )
+
+
 def main():
+
+
     raw_data = read_data()
     print("raw data:\n")
     print(raw_data)
-    formatted_data = format_data(raw_data)
-    print("formatted data:\n")
-    print(formatted_data)
+
+    # formatted_data = format_data(raw_data, max_features, max_len)
+    # print("formatted data:\n")
+    # print(formatted_data)
 
 
 def read_data():
@@ -19,8 +40,35 @@ def read_data():
     return data
 
 
-def format_data(raw_dataset):
-    return 0
+def format_data(data, max_features, maxlen, shuffle=False):
+    if shuffle:
+        data = data.sample(frac=1).reset_index(drop=True)
+
+        # Add start and end tokens
+    data['headline'] = START + ' ' + data['headline'].str.lower() + ' ' + END
+
+    text = data['headline']
+
+    # Tokenize text
+    filters = "!\"#$%&()*+,-./;<=>?@[\\]^_`{|}~\t\n"
+    tokenizer = Tokenizer(num_words=max_features, filters=filters)
+    tokenizer.fit_on_texts(list(text))
+    corpus = tokenizer.texts_to_sequences(text)
+
+    # Build training sequences of (context, next_word) pairs.
+    # Note that context sequences have variable length. An alternative
+    # to this approach is to parse the training data into n-grams.
+    X, Y = [], []
+    for line in corpus:
+        for i in range(1, len(line) - 1):
+            X.append(line[:i + 1])
+            Y.append(line[i + 1])
+
+    # Pad X and convert Y to categorical (Y consisted of integers)
+    X = pad_sequences(X, maxlen=maxlen)
+    Y = to_categorical(Y, num_classes=max_features)
+
+    return X, Y, tokenizer
 
 
 if __name__ == "__main__":
