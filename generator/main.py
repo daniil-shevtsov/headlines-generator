6e89c54c414166768d57f95b3645ab6f252f3fe4
Daniil Shevtsov <me@daniilshevtsov.com>
2020-12-20 14:28:04 +0300

    Add reading data

--- generator/main.py
+++ generator/main.py
@@ -1,14 +1,26 @@
 import pandas as pd
 
+START = '÷'
+END = '■'
+
 
 def main():
-    print("Hello World!")
+    raw_data = read_data()
+    print("raw data:\n")
+    print(raw_data)
+    formatted_data = format_data(raw_data)
+    print("formatted data:\n")
+    print(formatted_data)
 
 
 def read_data():
     dataset = pd.read_csv('../generator/dataset/dataset.csv', delimiter=',')
     data = dataset.drop(['publish_date'], axis=1).rename(columns={'headline_text': 'headline'})
     return data
+
+
+def format_data(raw_dataset):
+    return 0
 
 
 if __name__ == "__main__":
